3044928ce0527b298c770627b1547dcc2bb100ea
cosre7 <cosre7@gmail.com>
2024-04-11 16:06:50 +0900

    [feat] 로그인 페이지

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,4 @@
 export default function Button({clickEvent, step}) {
-    console.log(step)
     return (
         <div className="flex w-full absolute bottom-[32px] px-[16px]">
             <button onClick={clickEvent} className="bg-black text-white py-[18px] w-full font-semibold text-[22px] rounded-[16px]">
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
+import NavButton from "../components/NavButton";
+
 export default function Login() {
     return (
-        <div className="h-full w-full overflow-hidden flex flex-col items-center px-6">
-            <div className="w-full h-[519px] bg-gray-300 mt-32">
+        <div className="h-full w-full flex flex-col items-center px-6">
+            <div className="w-full h-[519px] bg-[#D9D9D9] mt-[34px]">
             </div>
-            <div className="flex flex-col mt-10 w-full">
-                <button className="w-full bg-black text-white text-2xl py-3 rounded-[16px]">Google로 가입하기</button>
-                <button className="mt-5">이메일로 가입하기</button>
+            <NavButton title={'Google로 가입하기'}/>
+            <div className="absolute bottom-0">
+                <button className="mt-[16px] mb-[74px]">이메일로 가입하기</button>
             </div>
         </div>
     );
