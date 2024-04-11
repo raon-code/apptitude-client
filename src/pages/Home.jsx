ce633636bd2420965e5cdf24ce9ecec60e2d9d42
cosre7 <cosre7@gmail.com>
2024-04-11 19:52:24 +0900

    [feat] 모달창

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import Modal from "../components/Modal";
 import Nav from "../components/Nav";
 
 export default function Home() {
     return (
         <div className="">
-            <Nav />
+            <div className="">
+                <Nav />
+            </div>
+            <Modal />
         </div>
+
     );
 }
