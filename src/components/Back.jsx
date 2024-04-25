d778908952d039b23cc24d9e845834ed1684d803
yeahzzii <107247219+yeahzzii@users.noreply.github.com>
2024-04-25 17:11:23 +0900

    [feat] depth별 텍스트 추가

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,17 +1,100 @@
-export default function Back({title, backClick}) {
-    return (
-        <div>
-            <div onClick={backClick}>
-                <button className="absolute top-[14px] left-[16px]">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-                    <line x1="19" x2="5" y1="12" y2="12" />
-                    <polyline points="12 19 5 12 12 5" />
-                    </svg>
-                </button>
-            </div>
-            <div className="flex justify-center mt-[14px]">
-                {title && <span className="text-[20px] font-medium">{title}</span>}
-            </div>
-        </div>
-    );
+import styled from 'styled-components';
+
+const Header = styled.div`
+  background-color: var(--background_color, #050409);
+  color: rgba(254, 254, 254, 0.98);
+  padding: 8px 16px;
+  display: flex;
+  align-items: center; /* 세로 가운데 정렬 */
+  justify-content: space-between;
+`;
+
+const Status = styled.div`
+  position: absolute;
+  right: 50px; /* 오른쪽으로 16px 이동 */
+  display: flex;
+  justify-content: center;
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const SvgContainer = styled.div`
+  display: flex;
+  justify-content: center; /* 가로 중앙 정렬 */
+  align-items: center; /* 세로 중앙 정렬 */
+  width: 100%;
+`;
+
+export default function Back({ title, backClick }) {
+  return (
+    <Header>
+      <div onClick={backClick}>
+        <button>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            width='24'
+            height='24'
+            className='main-grid-item-icon'
+            fill='none'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+          >
+            <line x1='19' x2='5' y1='12' y2='12' />
+            <polyline points='12 19 5 12 12 5' />
+          </svg>
+        </button>
+      </div>
+      {title && <span>{title}</span>}
+      <SvgContainer>
+        <Status>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='12'
+            height='18'
+            viewBox='0 0 12 18'
+            fill='none'
+          >
+            <circle cx='10' cy='10.5' r='10' fill='#28272B' />
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='12'
+              height='18'
+              viewBox='0 0 12 18'
+              fill='none'
+            >
+              <mask
+                id='path-1-outside-1_3_101'
+                maskUnits='userSpaceOnUse'
+                x='-9.17939e-07'
+                y='0.499999'
+                width='12'
+                height='18'
+                fill='black'
+              >
+                <rect
+                  fill='white'
+                  x='-9.17939e-07'
+                  y='0.499999'
+                  width='12'
+                  height='18'
+                />
+                <path d='M0.999999 1.5C2.75537 1.5 4.47981 1.96206 6 2.83975C7.52019 3.71743 8.78257 4.97981 9.66025 6.5C10.5379 8.02019 11 9.74463 11 11.5C11 13.2554 10.5379 14.9798 9.66025 16.5L1 11.5L0.999999 1.5Z' />
+              </mask>
+              <path
+                d='M0.999999 1.5C2.75537 1.5 4.47981 1.96206 6 2.83975C7.52019 3.71743 8.78257 4.97981 9.66025 6.5C10.5379 8.02019 11 9.74463 11 11.5C11 13.2554 10.5379 14.9798 9.66025 16.5L1 11.5L0.999999 1.5Z'
+                stroke='#04C357'
+                strokeWidth='2'
+                mask='url(#path-1-outside-1_3_101)'
+              />
+            </svg>
+          </svg>
+        </Status>
+      </SvgContainer>
+    </Header>
+  );
 }
--- src/pages/MakeBattle.jsx
+++ src/pages/MakeBattle.jsx
@@ -1,44 +1,67 @@
-import { useState } from "react";
-import Back from "../components/Back";
-import Button from "../components/Button";
+import styled from 'styled-components';
+import { useState } from 'react';
+import Back from '../components/Back';
+import Button from '../components/Button';
 
-const titles = ['대결 이름', '대결 목표', '대결 보상', '대결 확인']
+const titles = ['대결 이름', '대결 목표', '대결 보상', '대결 확인'];
+const stepInfo = [
+  '1:1 대결방의 이름과 기간을 설정해주세요!',
+  '디톡스 시간을 설정해 주세요!',
+  '대결 보상을 설정해주세요!'
+];
+
+const FieldWrap = styled.div`
+  width: 100vw;
+  height: calc(100vh - 45px);
+`;
+const StepInfo = styled.h2`
+  font-size: 20px;
+  color: #333;
+  display: flex;
+  justify-content: center;
+`;
 
 export default function Battle() {
-    const [index, setIndex] = useState(1);
-    const [title, setTitle] = useState(titles[0]);
-    const [step, setStep] = useState('다음');
-    const [confirm, setConfirm] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [title, setTitle] = useState(titles[index]);
+  const [step, setStep] = useState('다음');
+  const [confirm, setConfirm] = useState(true);
 
-    function onClick() {
-        if (titles.length === index) {
-            console.log('끝')
-            return
-        }
-        setIndex((crr) => crr + 1);
-        setTitle(titles[index])
-        if (index === 3) {
-            setStep('확인했습니다!')
-        }
+  function onClick() {
+    if (titles.length === index + 1) {
+      console.log('끝');
+      return;
     }
-
-    function backClick() {
-        console.log(index)
-        if (index === 1) {
-            window.history.back()
-        }         
-        setIndex((crr) => crr - 1);
-        setTitle(titles[index - 2]);
-        if (index !== 3) {
-            setStep('다음!')
-        }
-
+    setIndex((prevIndex) => prevIndex + 1);
+    setTitle(titles[index + 1]);
+    if (index === 2) {
+      setStep('확인했습니다!');
     }
+  }
 
-    return (
-        <div>
-            <Back title={title} backClick={backClick}/>
-            <Button clickEvent={onClick} step={step} confirm={confirm}/>
-        </div>
-    );
+  function backClick() {
+    console.log(index);
+    if (index === 0) {
+      window.history.back();
+    }
+    setIndex((prevIndex) => prevIndex - 1);
+    setTitle(titles[index - 1]);
+    if (index !== 2) {
+      setStep('다음!');
+    }
+  }
+
+  return (
+    <div>
+      <Back
+        title={title}
+        backClick={backClick}
+        style={{ backgroundColor: '#000' }}
+      />
+      <FieldWrap>
+        <StepInfo>{stepInfo[index]}</StepInfo>
+      </FieldWrap>
+      <Button clickEvent={onClick} step={step} confirm={confirm} />
+    </div>
+  );
 }
