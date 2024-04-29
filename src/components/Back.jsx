920805537a2fe289a152bd62aca0cf31b838615d
yeahzzii <107247219+yeahzzii@users.noreply.github.com>
2024-04-29 14:30:26 +0900

    [feat] 대결방 만들기 header status 이미지 추가

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -7,27 +7,24 @@ const Header = styled.div`
   display: flex;
   align-items: center; /* 세로 가운데 정렬 */
   justify-content: space-between;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+  border: none !important;
 `;
 
 const Status = styled.div`
-  position: absolute;
-  right: 50px; /* 오른쪽으로 16px 이동 */
-  display: flex;
-  justify-content: center;
-  svg {
-    width: 20px;
-    height: 20px;
-  }
+  background-image: ${(props) =>
+    `url(${props.statusImage})`}; // background-image에 props를 이용하여 동적으로 URL 설정
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-size: 20px;
+  background-position: center;
 `;
 
-const SvgContainer = styled.div`
-  display: flex;
-  justify-content: center; /* 가로 중앙 정렬 */
-  align-items: center; /* 세로 중앙 정렬 */
-  width: 100%;
-`;
-
-export default function Back({ title, backClick }) {
+export default function Back({ title, backClick, statusImage }) {
   return (
     <Header>
       <div onClick={backClick}>
@@ -50,51 +47,7 @@ export default function Back({ title, backClick }) {
         </button>
       </div>
       {title && <span>{title}</span>}
-      <SvgContainer>
-        <Status>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='12'
-            height='18'
-            viewBox='0 0 12 18'
-            fill='none'
-          >
-            <circle cx='10' cy='10.5' r='10' fill='#28272B' />
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='12'
-              height='18'
-              viewBox='0 0 12 18'
-              fill='none'
-            >
-              <mask
-                id='path-1-outside-1_3_101'
-                maskUnits='userSpaceOnUse'
-                x='-9.17939e-07'
-                y='0.499999'
-                width='12'
-                height='18'
-                fill='black'
-              >
-                <rect
-                  fill='white'
-                  x='-9.17939e-07'
-                  y='0.499999'
-                  width='12'
-                  height='18'
-                />
-                <path d='M0.999999 1.5C2.75537 1.5 4.47981 1.96206 6 2.83975C7.52019 3.71743 8.78257 4.97981 9.66025 6.5C10.5379 8.02019 11 9.74463 11 11.5C11 13.2554 10.5379 14.9798 9.66025 16.5L1 11.5L0.999999 1.5Z' />
-              </mask>
-              <path
-                d='M0.999999 1.5C2.75537 1.5 4.47981 1.96206 6 2.83975C7.52019 3.71743 8.78257 4.97981 9.66025 6.5C10.5379 8.02019 11 9.74463 11 11.5C11 13.2554 10.5379 14.9798 9.66025 16.5L1 11.5L0.999999 1.5Z'
-                stroke='#04C357'
-                strokeWidth='2'
-                mask='url(#path-1-outside-1_3_101)'
-              />
-            </svg>
-          </svg>
-        </Status>
-      </SvgContainer>
+      <Status statusImage={statusImage} /> {/* statusImage를 props로 전달 */}
     </Header>
   );
 }
