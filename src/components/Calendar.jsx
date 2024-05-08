5147f43874bdb19bd02acd94ff0f37f502eb68a9
yeahzzii <107247219+yeahzzii@users.noreply.github.com>
2024-05-08 16:16:28 +0900

    [refactor] calendar 수정

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
-import styled from "styled-components";
+import styled from 'styled-components';
 import 'react-calendar/dist/Calendar.css';
-import moment from "moment";
+import moment from 'moment';
 
 const BattleCalendar = ({ onChange, value }) => {
   const CalendarContainer = styled.div`
     position: relative;
+    width: 100%;
   `;
 
   const DropdownButton = styled.button`
-    width: 200px;
-    height: 48px;
-    border: 0.8px solid var(--festie-gray-600, #949494);
+    width: 100%;
+    padding: 16px;
     border-radius: 10px;
-    padding: 0px 12px;
-    color: var(--festie-gray-800, #3a3a3a);
-    font-family: SUIT Variable;
-    font-size: 16px;
-    font-style: normal;
+    background-color: var(--inputBox_color, #28272b);
+    color: #888;
+    font-size: 18px;
     font-weight: 500;
-    line-height: 140%;
-    text-align: start;
-    appearance: none;
-    background-color: white;
-    background-repeat: no-repeat;
-    background-position: right 12px center;
-    background-size: 12px;
+    letter-spacing: -0.45px;
   `;
 
   const CalendarWrapper = styled.div`
@@ -34,27 +26,60 @@ const BattleCalendar = ({ onChange, value }) => {
     position: absolute;
     top: 100%;
     left: 0;
-    display: ${(props) => (props.isOpen ? "block" : "none")};
+    display: ${(props) => (props.isOpen ? 'block' : 'none')};
+    .react-calendar__tile--now {
+      background: #ef5350;
+      color: #fafafa;
+    }
+
+    .react-calendar__tile--now:enabled:hover,
+    .react-calendar__tile--now:enabled:focus {
+      background: #ef5350;
+      color: #fafafa;
+    }
+    .react-calendar__tile--active {
+      background: #7fb77e;
+      color: white;
+    }
+    .react-calendar__tile--active:enabled:hover,
+    .react-calendar__tile--active:enabled:focus {
+      background: #7fb77e;
+    }
   `;
 
-  const [nowDate, setNowDate] = useState("날짜");
+  const [startDate, setStartDate] = useState('언제부터 언제까지 대결하나요?');
+  const [endDate, setEndDate] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggleCalendar = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleDateChange = (selectedDate) => {
-    onChange(selectedDate);
-    setIsOpen(false);
-    setNowDate(moment(selectedDate).format("YYYY년 MM월 DD일"));
+  // const handleDateChange = (selectedDate) => {
+  //   onChange(selectedDate);
+  //   setIsOpen(false);
+  //   setNowDate(moment(selectedDate).format('YYYY년 MM월 DD일'));
+  // };
+
+  const changeDate = (e) => {
+    const startDateFormat = moment(e[0]).format('YYYY.MM.DD');
+    const endDateFormat = moment(e[1]).format('YYYY.MM.DD');
+    // 여행 시작일자와 마치는일자의 값이 변할 때마다 값을 다시 세팅해줌
+    setStartDate(startDateFormat);
+    setEndDate(endDateFormat);
   };
 
   return (
     <CalendarContainer>
-      <DropdownButton onClick={handleToggleCalendar}>{nowDate}</DropdownButton>
+      <DropdownButton onClick={handleToggleCalendar}>
+        {startDate} {endDate}
+      </DropdownButton>
       <CalendarWrapper isOpen={isOpen}>
-        <Calendar onChange={handleDateChange} value={value}></Calendar>
+        <Calendar
+          onChange={changeDate} // 적용된 부분
+          selectRange={true}
+          formatDay={(locale, date) => moment(date).format('DD')}
+        ></Calendar>
       </CalendarWrapper>
     </CalendarContainer>
   );
--- src/pages/MakeBattle.jsx
+++ src/pages/MakeBattle.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import Back from '../components/Back';
 import Button from '../components/Button';
+import BattleCalendar from '../components/Calendar';
+import TimePicker from '../components/TimePicker';
 
 const titles = ['대결 이름', '대결 목표', '대결 보상', '대결 확인'];
 const stepInfo = [
@@ -27,6 +29,12 @@ const placeholders = [
   '이름은 최대 20자입니다.'
 ];
 const stepTitles = ['대결 이름', '대결 기간', '대결 보상', '대결 확인'];
+const randomBattleName = [
+  '더이상 이렇게 살 수는 없다',
+  '스마트폰 중독자 탈출 챌린지',
+  '디낳괴'
+];
+const randomBenefit = ['떡볶이 쏘기', '치킨 쏘기', '송금 100만원'];
 
 const FieldWrap = styled.div`
   width: 100vw;
@@ -34,6 +42,9 @@ const FieldWrap = styled.div`
   background-color: #050409;
   padding: 64px 16px 0;
   border: none;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
 `;
 const StepInfo = styled.h2`
   display: flex;
@@ -46,20 +57,21 @@ const StepInfo = styled.h2`
   font-weight: 500;
   line-height: 34px; /* 154.545% */
   letter-spacing: -0.55px;
-  margin-bottom: 31px;
+  /* margin-bottom: 31px; */
 `;
 const InputField = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 12px;
-  margin-bottom: 32px;
+  /* margin-bottom: 32px; */
 `;
 const StepInput = styled.input`
   width: 100%;
   padding: 16px;
   border-radius: 10px;
   background-color: var(--inputBox_color, #28272b);
+  color: #fefefe;
 `;
 const InputTitle = styled.h4`
   color: #bebdc4;
@@ -83,6 +95,8 @@ const RandomButton = styled.div`
     font-size: 14px;
     font-weight: 500;
     letter-spacing: -0.35px;
+    border-radius: 30px;
+    background-color: var(--inputBox_color, #28272b);
   }
 
   span {
@@ -93,7 +107,33 @@ const RandomButton = styled.div`
     display: inline-block;
   }
 `;
-
+const BattleTime = styled.div`
+  color: #bebdc4;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 500;
+  letter-spacing: -0.4px;
+  p {
+    padding: 16px;
+    color: #fefefe;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 34px; /* 188.889% */
+    letter-spacing: -0.45px;
+    border-radius: 10px;
+    border: 1px solid #fff;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    margin-top: 32px;
+  }
+  span {
+    background-image: url('../../../Hhourglass_move_light_fill.svg');
+    width: 24px;
+    height: 24px;
+    display: inline-block;
+  }
+`;
 export default function Battle() {
   const [index, setIndex] = useState(0);
   const [title, setTitle] = useState(titles[index]);
@@ -102,6 +142,14 @@ export default function Battle() {
   const [inputValue, setInputValue] = useState('');
   const [statusImage, setStatusImage] = useState('../../../status_1:3.svg');
   const [placeholder, setPlaceholder] = useState(placeholders[index]);
+  const [selectedTime, setSelectedTime] = useState({ hour: 0, minute: 0 }); // State to store selected time
+
+  const handleSetHours = (selectedHour) => {
+    console.log('Selected hour:', selectedHour);
+  };
+  const handleSetMinutes = (minutes) => {
+    console.log('Selected minutes:', minutes);
+  };
 
   function onClick() {
     if (titles.length === index + 1) {
@@ -112,6 +160,7 @@ export default function Battle() {
     setTitle(titles[index + 1]);
     setPlaceholder(placeholders[index + 1]);
     if (index === 2) {
+      console.log(index);
       setStep('확인했습니다!');
     }
     setStatusImage(`../../../status_${index + 2}:3.svg`);
@@ -131,6 +180,10 @@ export default function Battle() {
     setStatusImage(`../../../status_${index}:3.svg`);
   }
 
+  function getRandomItemFromArray(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
   return (
     <div>
       <Back
@@ -143,18 +196,53 @@ export default function Battle() {
         <StepInfo>{stepInfo[index]}</StepInfo>
         <InputField>
           <InputTitle className='inputTitle'>{stepTitles[index]}</InputTitle>
-          <StepInput
-            type='text'
-            placeholder={placeholder}
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
+          {index === 1 ? (
+            <BattleCalendar
+              onChange={(selectedDate) => {
+                // 선택한 날짜 범위를 상태로 업데이트
+                console.log(selectedDate);
+              }}
+              value={{ startDate: new Date(), endDate: new Date() }} // 초기값 설정
+            />
+          ) : (
+            <StepInput
+              type='text'
+              placeholder={placeholder}
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          )}
         </InputField>
-        <RandomButton>
-          <button>
-            랜덤으로 뽑기<span></span>
-          </button>
-        </RandomButton>
+        {index === 1 ? (
+          <BattleTime>
+            <InputTitle className='inputTitle'>하루 디톡스 시간</InputTitle>
+            <TimePicker
+              setHours={(hour) => setSelectedTime({ ...selectedTime, hour })} // Callback to update selected hour
+              setMinutes={(minute) =>
+                setSelectedTime({ ...selectedTime, minute })
+              }
+            />
+            {/* 시간 설정 */}
+            <p>
+              <span></span>
+              {`총 디톡스 시간은 ${selectedTime.hour}시간 ${selectedTime.minute}분 입니다.`}
+            </p>
+          </BattleTime>
+        ) : (
+          <RandomButton>
+            <button
+              onClick={() => {
+                if (index === 2) {
+                  setInputValue(getRandomItemFromArray(randomBenefit));
+                } else {
+                  setInputValue(getRandomItemFromArray(randomBattleName));
+                }
+              }}
+            >
+              랜덤으로 뽑기<span></span>
+            </button>
+          </RandomButton>
+        )}
       </FieldWrap>
       <Button clickEvent={onClick} step={step} confirm={confirm} />
     </div>
