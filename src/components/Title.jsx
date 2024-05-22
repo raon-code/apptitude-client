0cd335e569f9460371783c1cff4ee70d184d3cd8
cosre7 <cosre7@gmail.com>
2024-05-22 17:12:18 +0900

    [refactor] leading 추가

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ title }) {
   return (
-    <div className='text-[#FEFEFE] flex items-center justify-center text-[20px] opacity-[0.98] mt-[8px]'>
+    <div className='text-[#FEFEFE] flex items-center justify-center text-[20px] leading-[26px] opacity-[0.98] mt-[8px]'>
       <span>{title}</span>
     </div>
   );
