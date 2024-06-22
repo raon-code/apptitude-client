16ee1464134e6bf47cfd737c620bca546cb9ee65
seokjw <wldn9020@daum.net>
2024-06-22 23:37:47 +0900

    [refactor] 이미지와 슬라이드 위치 조정

--- src/components/OnboardingSlides.jsx
+++ src/components/OnboardingSlides.jsx
@@ -10,31 +10,33 @@ export default function OnboardingSlides({ settings, setCurrentSlide }) {
   };
 
   return (
-    <Slider {...updatedSettings} className='h-full w-full'>
-      <div className='w-full my-[80px] flex flex-col items-center justify-center'>
-        <div className='flex flex-col items-center justify-center'>
-          <img src={image1} alt='Slide 1' className='w-1/2 h-auto' />
-          <h2 className='text-white text-2xl mt-4 text-center'>
-            디지털 디톡스, <br /> 혼자하기 어려우시죠?
-          </h2>
+    <div className='h-full w-full'>
+      <Slider {...updatedSettings}>
+        <div className='w-full my-[80px] flex flex-col items-center justify-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={image1} alt='Slide 1' className='w-1/2 h-auto' />
+            <h2 className='text-[#FEFEFE] text-[22px] mt-4 text-center'>
+              디지털 디톡스, <br /> 혼자하기 어려우시죠?
+            </h2>
+          </div>
         </div>
-      </div>
-      <div className='w-full my-[80px] flex flex-col items-center justify-center'>
-        <div className='flex flex-col items-center justify-center'>
-          <img src={image2} alt='Slide 2' className='w-1/2 h-auto' />
-          <h2 className='text-white text-2xl mt-4 text-center'>
-            친구와 1:1로 경쟁하며 <br /> 디톡스를 해보세요!
-          </h2>
+        <div className='w-full my-[80px] flex flex-col items-center justify-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={image2} alt='Slide 2' className='w-1/2 h-auto' />
+            <h2 className='text-[#FEFEFE] text-[22px] mt-4 text-center'>
+              친구와 1:1로 경쟁하며 <br /> 디톡스를 해보세요!
+            </h2>
+          </div>
         </div>
-      </div>
-      <div className='w-full my-[80px] flex flex-col items-center justify-center'>
-        <div className='flex flex-col items-center justify-center'>
-          <img src={image3} alt='Slide 3' className='w-1/2 h-auto' />
-          <h2 className='text-white text-2xl mt-4 text-center'>
-            가벼운 내기 상품도 건다면, <br /> 더 재미있겠죠?
-          </h2>
+        <div className='w-full my-[80px] flex flex-col items-center justify-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <img src={image3} alt='Slide 3' className='w-1/2 h-auto' />
+            <h2 className='text-[#FEFEFE] text-[22px] mt-4 text-center'>
+              가벼운 내기 상품도 건다면, <br /> 더 재미있겠죠?
+            </h2>
+          </div>
         </div>
-      </div>
-    </Slider>
+      </Slider>
+    </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,34 +18,35 @@ export default function Login() {
 
   return (
     <div className='h-full w-full flex flex-col items-center px-6 py-6'>
-      <div>
-        {currentSlide === 0 && (
-          <img src={logo} alt='Logo' className='mt-3 mb-3' />
-        )}
-        {(currentSlide === 1 || currentSlide === 2) && (
-          <img src={icon} alt='icon' className='mt-2.5 mb-2.5' />
-        )}
+      <div className='fixed top-6 w-full flex justify-center'>
+        <div>
+          {currentSlide === 0 && <img src={logo} alt='Logo' />}
+          {(currentSlide === 1 || currentSlide === 2) && (
+            <img src={icon} alt='icon' className='mt-6' style={{ zIndex: 1 }} />
+          )}
+        </div>
       </div>
-      <div className='w-full bg-[#28272B] rounded-[18px] flex items-center justify-center overflow-hidden'>
+      <div
+        className='w-full bg-[#28272B] rounded-[16px] flex items-center justify-center overflow-hidden mt-20'
+        style={{ zIndex: 0 }}
+      >
         <OnboardingSlides
           settings={settings}
           setCurrentSlide={setCurrentSlide}
         />
       </div>
-      <div>
-        <ul className='slick-custom'>
-          {[0, 1, 2].map((index) => (
-            <li
-              key={index}
-              className={index === currentSlide ? 'slick-active' : ''}
-            >
-              <button onClick={() => setCurrentSlide(index)}>
-                <span></span>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ul className='slick-custom'>
+        {[0, 1, 2].map((index) => (
+          <li
+            key={index}
+            className={index === currentSlide ? 'slick-active' : ''}
+          >
+            <button onClick={() => setCurrentSlide(index)}>
+              <span></span>
+            </button>
+          </li>
+        ))}
+      </ul>
       <div className='bottom-6 w-full fixed justify-center'>
         <LoginButton title={'kakao로 로그인'} />
       </div>
